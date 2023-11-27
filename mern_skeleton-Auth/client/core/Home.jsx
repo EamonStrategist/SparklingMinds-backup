d1211f896256e75813f749ba60e600bd25bb9bcc
nsaha7 <trinasaha1234@gmail.com>
2023-11-27 14:51:41 -0500

    extra changes

--- mern_skeleton-Auth/client/core/Home.jsx
+++ mern_skeleton-Auth/client/core/Home.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import LogoImage from './../assets/images/Logo.png';
-import { Link } from 'react-router-dom';
-import './Home.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import LogoImage from "./../assets/images/Logo.png";
+import { Link } from "react-router-dom";
+import "./Home.css";
+import SparklingMindsLogo from "./../assets/images/Logo.png";
 
 const useStyles = makeStyles((theme) => ({
   card: {
     maxWidth: 600,
-    margin: 'auto',
+    margin: "auto",
     marginTop: theme.spacing(5),
   },
   title: {
@@ -19,96 +20,108 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.openTitle,
   },
   media: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     minHeight: 400,
-    width: '50%',
-    margin: '0 auto',
-    height: '100%',
-    objectFit: 'cover',
+    width: "50%",
+    margin: "0 auto",
+    height: "100%",
+    objectFit: "cover",
     marginBottom: theme.spacing(2),
   },
   navbar: {
-    backgroundColor: '#333',
-    padding: '15px',
-    marginBottom: '20px',
-    display: 'flex',
-    justifyContent: 'space-between', // Adjusted to space out the links and cart
-    alignItems: 'center', // Center items vertically
+    backgroundColor: "#333",
+    padding: "15px",
+    marginBottom: "20px",
+    display: "flex",
+    justifyContent: "space-between", // Adjusted to space out the links and cart
+    alignItems: "center", // Center items vertically
   },
   navbarLink: {
-    color: 'white',
-    textDecoration: 'none',
-    margin: '0 15px',
-    fontWeight: 'bold',
-    transition: 'color 0.3s ease-in-out',
+    color: "white",
+    textDecoration: "none",
+    margin: "0 15px",
+    fontWeight: "bold",
+    transition: "color 0.3s ease-in-out",
   },
   navbarLinkHover: {
-    textDecoration: 'underline',
+    textDecoration: "underline",
   },
   cartIcon: {
-    color: 'white',
+    color: "white",
     fontSize: 24,
-    cursor: 'pointer',
-    transition: 'color 0.3s ease-in-out',
+    cursor: "pointer",
+    transition: "color 0.3s ease-in-out",
   },
   cartIconHover: {
-    color: '#e44d26',
+    color: "#e44d26",
   },
 }));
 
 export default function Home() {
   const classes = useStyles();
 
-  // Replace cartIcon with your desired icon or image for the cart
-  const cartIcon = '🛒';
+  const cartIcon = "🛒";
 
   return (
     <>
       {/* Render the navbar */}
       <div className={classes.navbar}>
         <div>
-          <Link to="/" className={classes.navbarLink}>
-            Home
-          </Link>{' '}
-          |{' '}
-          <Link to="/users" className={classes.navbarLink}>
-            Users
-          </Link>{' '}
-          |{' '}
-          <Link to="/signup" className={classes.navbarLink}>
-            Signup
-          </Link>{' '}
-          |{' '}
-          <Link to="/signin" className={classes.navbarLink}>
-            Signin
-          </Link>{' '}
-          |{' '}
-          <Link to="/" className={classes.navbarLink}>
-            Sign-out
-          </Link>{' '}
-          |{' '}
-          <Link to="/about" className={classes.navbarLink}>
-            About
-          </Link>{' '}
-          |{' '}
-          <Link to="/products" className={classes.navbarLink}>
-            Products
-          </Link>{' '}
-          |{' '}
-          <Link to="/contact" className={classes.navbarLink}>
-            Contact
-          </Link>{' '}
-          |{' '}
-          <Link to="/admin" className={classes.navbarLink}>
-            Admin
-          </Link>
-          <Link to="/testpage" className={classes.navbarLink}>
-            Security Test
-          </Link>
+          {/* Add the Sparkling Minds logo */}
+          <img
+            src={SparklingMindsLogo}
+            alt="Sparkling Minds Logo"
+            className={classes.logoImage}
+          />
+          {/* Render the navbar */}
+          <div className={classes.navbar}>
+            <div>
+              <Link to="/" className={classes.navbarLink}>
+                Home
+              </Link>{" "}
+              |{" "}
+              <Link to="/users" className={classes.navbarLink}>
+                Users
+              </Link>{" "}
+              |{" "}
+              <Link to="/signup" className={classes.navbarLink}>
+                Signup
+              </Link>{" "}
+              |{" "}
+              <Link to="/signin" className={classes.navbarLink}>
+                Signin
+              </Link>{" "}
+              |{" "}
+              <Link to="/" className={classes.navbarLink}>
+                Sign-out
+              </Link>{" "}
+              |{" "}
+              <Link to="/about" className={classes.navbarLink}>
+                About
+              </Link>{" "}
+              |{" "}
+              <Link to="/products" className={classes.navbarLink}>
+                Products
+              </Link>{" "}
+              |{" "}
+              <Link to="/contact" className={classes.navbarLink}>
+                Contact
+              </Link>{" "}
+              |{" "}
+              <Link to="/admin" className={classes.navbarLink}>
+                Admin
+              </Link>
+              <Link to="/testpage" className={classes.navbarLink}>
+                Security Test
+              </Link>
+            </div>
+            {/* Cart Icon */}
+            <div className={`${classes.cartIcon} ${classes.cartIconHover}`}>
+              {cartIcon}
+            </div>
+          </div>
         </div>
-        {/* Cart Icon */}
-        <div className={`${classes.cartIcon} ${classes.cartIconHover}`}>{cartIcon}</div>
       </div>
 
       {/* Render the card */}
@@ -116,14 +129,54 @@ export default function Home() {
         <Typography variant="h6" className={classes.title}>
           Sparkling Minds
         </Typography>
-        <CardMedia className={classes.media} image={LogoImage} title="Sparkling Minds" />
+        <CardMedia
+          className={classes.media}
+          image={LogoImage}
+          title="Sparkling Minds"
+        />
         <CardContent>
           <Typography variant="body2" component="p">
-            Sparkle in Every Story. Find Your Glitter. Find that special something for that special someone, from
-            simple to statement to sentimental. Shop our top categories.
+            Sparkle in Every Story. Find Your Glitter. Find that special
+            something for that special someone, from simple to statement to
+            sentimental. Shop our top categories.
           </Typography>
         </CardContent>
       </Card>
+      {/* Footer */}
+      <footer className="footer">
+        <div className="social-links">
+          {/* Social media icons */}
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/images/instagram.jpg" alt="Instagram" />
+          </a>
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/images/facebook.png" alt="Facebook" />
+          </a>
+        </div>
+        {/* Gmail icon for more info */}
+        <div className="gmail-icon">
+          <i className="fa fa-envelope" aria-hidden="true"></i>
+        </div>
+        {/* Contact Information */}
+        <div className="contact-info">
+          <p>Contact us:</p>
+          <p>Phone: (123) 456-7890</p>
+          <p>Email: info@example.com</p>
+        </div>
+
+        {/* Copyright */}
+        <div className="copyright">
+          <p>&copy; 2023 Your Company. All rights reserved.</p>
+        </div>
+      </footer>
     </>
   );
 }
